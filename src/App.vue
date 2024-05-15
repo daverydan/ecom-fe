0240ad5a2e59eb5bfb82eaee125a394466d8d36c
daverydan <danny.w.avery@gmail.com>
2024-05-15 13:50:37 -0400

    Add vue router & views

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
-  <div class="container py-4 px-3 mx-auto">
-    <h1>Hello, Bootstrap and Vite!</h1>
-
-    <button class="btn btn-primary d-flex">
-      <i class="bi bi-check me-1"></i>
-      Primary button
-    </button>
-  </div>
+  <main>
+    <RouterView />
+  </main>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router'
 
 import '../src/scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')

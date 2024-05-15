dba89e77d963ba19ff87926b4834852620efeff6
daverydan <danny.w.avery@gmail.com>
2024-05-15 12:06:42 -0400

    Add bootstrap with icons

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import '../src/scss/styles.scss'
+import * as bootstrap from 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 createApp(App).mount('#app')
